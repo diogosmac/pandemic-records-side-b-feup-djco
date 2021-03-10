12d27cb0a8ae4a104164f933763e5489b7a58b12
diogosmac <diogo.smac@gmail.com>
2021-03-10 19:31:45 +0000

    Blaster and Shotgun are operational

--- testing-template/GameSource/SlimeShooter/Scripts/MissileShotgun.gd
+++ testing-template/GameSource/SlimeShooter/Scripts/MissileShotgun.gd
@@ -1,21 +1,26 @@
 extends Area2D
 
-export(int) var Speed = 1000
+export(int) var Speed = 800
+
+export(int) var BlastRange = 200
 
 var angle
 
+var initPosition
+
 func _ready():
 	
+	initPosition = position
 	angle = Vector2( cos(rotation), sin(rotation) )
 
 
 func _physics_process(delta):
 	
 	position += (angle * Speed) * delta
+	if (position.distance_to(initPosition) > BlastRange):
+		self.queue_free()
 
 
 func _on_Missile_body_entered(enemy):
 	
 	enemy.missileHit()
-	
-	self.queue_free()
